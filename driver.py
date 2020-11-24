d1cb728fecc2829992cd782b18ce95b7b339f3f8
pavan <48682313+pavan-aeturi@users.noreply.github.com>
2020-11-24 18:25:17 +0530

    driverfile written

---
+++ driver.py
@@ -0,0 +1,45 @@
+from Node import Node
+from moves import *
+import random
+from Tree import Tree
+
+def search(root,sum,maxdepth=2):
+    print(root.mtx)
+    if root.totalSum==sum:
+        print("found")
+        return True
+    if root.totalSum>sum:
+        print("backtrack")
+        return False
+    else:
+        root.children=root.buildChildren(1)
+        l=list(root.children.keys())
+        random.shuffle(l)
+        for key in l:
+            possibleMoves=root.children[key]
+            random.shuffle(possibleMoves)
+            for i in range(len(possibleMoves)):
+                print(key)
+                if search(root.children[key][i],sum):
+                    return True
+        return False
+
+def insert_tile(mtx):
+	l=[]
+	for i in range(4):
+		for j in range(4):
+			if mtx[i][j]==0:
+				l.append([i,j])
+	if len(l)>0:
+		r=random.randint(0,len(l)-1)
+		mtx[l[r][0]][l[r][1]]=random.choice([2,4])
+
+def main():
+	mtx=[[0 for _ in range(4)] for i in range(4)]
+	insert_tile(mtx)
+	insert_tile(mtx)
+	tree=Tree(mtx)
+	search(tree.root,8)
+
+if __name__=="__main__":
+	main()
